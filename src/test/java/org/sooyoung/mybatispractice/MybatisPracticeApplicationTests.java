79e8457d31fe70bf666bcc690dea12017d5b91e6
Sooyoung <swimkim@icon.foundation>
2019-05-29 17:48:00 +0900

    first commit

---
+++ src/test/java/org/sooyoung/mybatispractice/MybatisPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sooyoung.mybatispractice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisPracticeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
